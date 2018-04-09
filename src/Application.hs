4b05c8ca6ddc23054d815a471d3c7c8aa74b1668
Dino Morelli <dino@ui3.info>
2018-04-09 15:26:43 -0400

    Removed comment scaffold example code and files

--- src/Application.hs
+++ src/Application.hs
@@ -43,7 +43,6 @@ import Yesod.Auth.Util.PasswordStore ( makePassword )
 -- Don't forget to add new modules to your cabal file!
 import Handler.Common
 import Handler.Home
-import Handler.Comment
 import Handler.Profile
 
 -- This line actually creates our YesodDispatch instance. It is the second half

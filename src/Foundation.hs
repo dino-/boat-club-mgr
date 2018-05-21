84c0dea43a86666975845cc1334e5dffd8d8e1e0
Dino Morelli <dino@ui3.info>
2018-05-21 14:41:41 -0400

    Reorganized the authentication check functions

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -160,10 +160,12 @@ instance Yesod App where
         -> Maybe (Route App)
     authRoute _ = Just $ AuthR LoginR
 
+
     isAuthorized
         :: Route App  -- ^ The route the user is visiting.
         -> Bool       -- ^ Whether or not this is a "write" request.
         -> Handler AuthResult
+
     -- Routes not requiring authentication.
     isAuthorized (AuthR _) _ = return Authorized
     isAuthorized HomeR _ = return Authorized
@@ -171,11 +173,13 @@ instance Yesod App where
     isAuthorized RobotsR _ = return Authorized
     isAuthorized (StaticR _) _ = return Authorized
 
-    -- the profile route requires that the user is authenticated, so we
-    -- delegate to that function
+    -- Routes requiring that the user is authenticated
     isAuthorized ProfileR _ = isAuthenticated
+
+    -- Routes requiring that an administrator is authenticated
     isAuthorized MembershipAddR _ = isAdmin
     isAuthorized UserListR _ = isAdmin
+
 
     -- This function creates static content files in the static folder
     -- and names them based on a hash of their content. This allows

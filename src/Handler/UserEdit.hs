2a2f1c844dc2459c9bd6887d2d2fd1b949854c98
Dino Morelli <dino@ui3.info>
2018-06-02 20:53:16 -0400

    Added form and code to edit user accounts
    Part of this code consolidates the Helper.UserEdit module into
    Handler.UserEdit. It's not used anywhere else.
    
    Also got rid of userFormEmpty because we always need to get the user record
    being edited from the db anyway, may as well just use that.
    
    Added isAdminB check to possibly disable editing of the Membership Number
    field. We only allow this for administrators. In the future, users will be able
    to edit their own records (but not that field).

--- src/Handler/UserEdit.hs
+++ src/Handler/UserEdit.hs
@@ -4,27 +4,66 @@
 
 module Handler.UserEdit where
 
+import Yesod.Form.Bootstrap3 (BootstrapFormLayout (BootstrapBasicForm), bfs
+  , renderBootstrap3)
+
 import Import
-import Helper.UserForm ( userForm, userFormEmpty )
+import Helper.User ( emptyUser )
+import Model.Types ( MbrNum (..), getMbrNum )
 
 
 getUserEditR :: UserId -> Handler Html
 getUserEditR userId = do
-  mUser <- runDB $ get userId
-  user <- maybe (redirect UserListR) return mUser
+  muser <- runDB $ get userId
+  user <- maybe (redirect UserListR) return muser
 
-  (widget, enctype) <- generateFormPost $ userForm user
+  isAdmin' <- isAdminB
+  (widget, enctype) <- generateFormPost $ userForm user isAdmin'
   formWrapper widget enctype Nothing userId
 
 
 postUserEditR :: UserId -> Handler Html
 postUserEditR userId = do
-  ((result, widget), enctype) <- runFormPost $ userFormEmpty
+  muser <- runDB $ get userId
+  user <- maybe (redirect UserListR) return muser
+
+  isAdmin' <- isAdminB
+  ((result, widget), enctype) <- runFormPost $ userForm user isAdmin'
+
   case result of
-    FormSuccess user -> do
-      _ <- runDB $ replace userId user
+    FormSuccess userFromForm -> do
+      -- Form values must be merged back into the existing record so we don't lose info
+      _ <- runDB $ replace userId $ user
+        { userMbrNum = userMbrNum userFromForm
+        , userGivenName = userGivenName userFromForm
+        , userSurName = userSurName userFromForm
+        }
+      setMessage $ toHtml ("User account successfully edited" :: Text)
       redirect HomeR
     _ -> formWrapper widget enctype (Just "Invalid input, let's try again.") userId
+
+
+userForm :: User -> Bool -> Form User
+userForm user isAdmin' = do
+  let disabledAttr = if isAdmin' then [] else [("disabled", "disabled")]
+  let fs = bfs ("Membership Number" :: Text)
+
+  renderBootstrap3 BootstrapBasicForm $ User
+    <$> aopt membershipNumField (fs { fsAttrs = fsAttrs fs ++ disabledAttr })
+      (Just . userMbrNum $ user)
+    <*> pure (userEmail emptyUser)
+    <*> pure (userPassword emptyUser)
+    <*> pure (userVerkey emptyUser)
+    <*> pure (userVerified emptyUser)
+    <*> areq textField (bfs ("First name" :: Text)) (Just . userGivenName $ user)
+    <*> areq textField (bfs ("Last name" :: Text)) (Just . userSurName $ user)
+
+  where
+    membershipNumErrMsg :: Text
+    membershipNumErrMsg = "Membership numbers must be > 0"
+
+    membershipNumField = convertField MbrNum getMbrNum
+      . checkBool (> 0) membershipNumErrMsg $ intField
 
 
 formWrapper :: Widget -> Enctype -> Maybe Text -> UserId -> HandlerFor App Html
